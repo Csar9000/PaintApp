51fb4177df5f38a912b4fb4731432b3231aca297
Csar9000 <72266005+Csar9000@users.noreply.github.com>
2023-04-20 20:37:31 +0800

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿
+namespace PaintApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(801, 646);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(827, 59);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 27);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(827, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Сторона трапеции";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(827, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Площадь";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Location = new System.Drawing.Point(827, 155);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(125, 27);
+            this.textBox2.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Crimson;
+            this.label1.Location = new System.Drawing.Point(827, 230);
+            this.label1.MaximumSize = new System.Drawing.Size(125, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 20);
+            this.label1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(982, 653);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.MaximumSize = new System.Drawing.Size(1000, 700);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace PaintApp
+{
+    public partial class Form1 : Form
+    {
+        public static double convertValue = 37.7952755905511;
+        public static double AD = 2 * convertValue;
+        public static double BC = 2 * AD;
+        public static int bitmapWidth = 0;
+        public static int bitmapHeight = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            drawFig(AD, BC, pictureBox1, textBox2);
+        }
+        public static double Distance(Point a, Point b)
+        {
+            double dX = a.X - b.X;
+            double dY = a.Y - b.Y;
+            return Math.Round(Math.Sqrt(dX * dX + dY * dY));
+        }
+
+        public static void drawFig(double AD, double BC, PictureBox pictureBox1, TextBox textBox2)
+        {
+            SolidBrush black = new SolidBrush(Color.Black);
+            SolidBrush white = new SolidBrush(Color.White);
+
+            // String[] namesPicks = new String[] { "A", "B", "C", "D", "E" };
+            Pen blackPen = new Pen(Color.White, 3);
+            Point[] points = new Point[4];// Массив точек трапеции.  
+            Point[] triangle = new Point[3];// Массив точек треугольника.
+
+
+            if (bitmapWidth == 0 && bitmapHeight == 0)
+            {
+                points[0].X = pictureBox1.Width / 2 - (int)Math.Round(AD, 0); points[0].Y = pictureBox1.Height / 2 - (int)Math.Round(AD, 0) - pictureBox1.Height / 8;
+                points[3].X = pictureBox1.Width / 2 + (int)Math.Round(AD, 0); points[3].Y = pictureBox1.Height / 2 - (int)Math.Round(AD, 0) - pictureBox1.Height / 8;
+
+                points[1].X = pictureBox1.Width / 2 - (int)Math.Round(BC, 0); points[1].Y = pictureBox1.Height / 2 + (int)Math.Round(BC, 0) - pictureBox1.Height / 8;
+                points[2].X = pictureBox1.Width / 2 + (int)Math.Round(BC, 0); points[2].Y = pictureBox1.Height / 2 + (int)Math.Round(BC, 0) - pictureBox1.Height / 8;
+            }
+            else
+            {
+                points[0].X = bitmapWidth / 2 - (int)Math.Round(AD, 0); points[0].Y = bitmapHeight / 2 - (int)Math.Round(AD, 0) - pictureBox1.Height / 8;
+                points[3].X = bitmapWidth / 2 + (int)Math.Round(AD, 0); points[3].Y = bitmapHeight / 2 - (int)Math.Round(AD, 0) - pictureBox1.Height / 8;
+
+                points[1].X = bitmapWidth / 2 - (int)Math.Round(BC, 0); points[1].Y = bitmapHeight / 2 + (int)Math.Round(BC, 0) - pictureBox1.Height / 8;
+                points[2].X = bitmapWidth / 2 + (int)Math.Round(BC, 0); points[2].Y = bitmapHeight / 2 + (int)Math.Round(BC, 0) - pictureBox1.Height / 8;
+            }
+
+
+            triangle[0].X = points[0].X; triangle[0].Y = points[0].Y;
+            triangle[1].X = points[1].X; triangle[1].Y = points[1].Y;
+            triangle[2].X = points[0].X; triangle[2].Y = triangle[1].Y;
+
+            // высчитывание заштрихованнной области без эллипса
+            double k1 = Distance(triangle[0], triangle[2]) + Distance(triangle[0], triangle[2]) / 1.6;
+            double k2 = Distance(triangle[2], triangle[1]) + Distance(triangle[2], triangle[1]) / 1.6;
+            double gip = Distance(triangle[0], triangle[1]) + Distance(triangle[0], triangle[1]) / 1.6;
+
+            // параметры заштрихованной области в сантиметрах
+            double tempk1 = k1 / convertValue;
+            double tempk2 = k2 / convertValue;
+            double tempGip = gip / convertValue;
+
+            // площадь треугольника
+            double squareTr = Math.Round((tempk1 * tempk2) / 2, 3);
+            // нахождение осей эллипса
+            double os1 = (tempk1 + tempk2 - tempGip) / 2;
+            double os2 = (tempk1 + tempk2 - tempGip);
+            // площадь эллипса
+            double squareEl = Math.Round(Math.PI * os1 * os2, 3);
+            textBox2.Text = Convert.ToString(Math.Round(squareTr - squareEl, 3)) + " см^2";
+
+            // параметры для располдожения эллипса
+            int X = triangle[2].X - (int)(k1 + k2 - gip) / 2;
+            int Y = triangle[1].Y - (int)(k1 + k2 - gip);
+            int width = (int)((k1 + k2 - gip) / 2) - 3;
+            int height = (int)(k1 + k2 - gip) - 3;
+
+            // эллипс
+            Rectangle rect = new Rectangle(X, Y, width, height);
+
+            // Изображение, которое будем вставлять в PictureBox.
+            Bitmap bmp = new Bitmap(pictureBox1.Width / 2 + (int)Distance(points[1], points[3]), pictureBox1.Height / 2 + (int)(Distance(triangle[0], triangle[2]))); // обработать ошибку!!!!
+
+            using (Graphics grfx = Graphics.FromImage(bmp))
+            {
+                grfx.Clear(Color.White);
+                grfx.DrawPolygon(Pens.Black, points);
+                grfx.FillPolygon(black, triangle);
+
+                /*for (int i = 0; i < points.Length; i++)
+                {
+                    grfx.DrawString(namesPicks[i].ToString(), new Font("Arial", 10), Brushes.Red, points[i].X - 20, points[i].Y - 1);
+                }*/
+
+
+                grfx.DrawEllipse(blackPen, rect);
+                grfx.FillEllipse(white, rect);
+            }
+
+            bitmapWidth = bmp.Width;
+            bitmapHeight = bmp.Height;
+            // Устанавливаем изображение.
+            pictureBox1.Image = bmp;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Contains('.'))
+            {
+                textBox1.Text = textBox1.Text.Replace('.', ',');
+                textBox1.SelectionStart = textBox1.Text.Length;
+            }
+            if (textBox1.Text.Length != 0)
+            {
+                if (double.TryParse(textBox1.Text, out double res) == true)
+                {
+                    label1.Text = "";
+                    if (Convert.ToDouble(textBox1.Text) <= 0)
+                    {
+                        label1.Text = "Необходимо ввести положительное число больше нуля";
+                        Graphics g = pictureBox1.CreateGraphics();
+                        g.Clear(Color.White);
+                        textBox2.Text = "0";
+                        AD = 0;
+                        BC = 0;
+                    }
+                    else
+                    {
+                        label1.Text = "";
+                        AD = Convert.ToDouble(textBox1.Text) * convertValue;
+                        BC = AD * 2;
+
+                        if (BC > pictureBox1.Width)
+                        {
+                            label1.Text = "Ширина картинки превышает размер окна.\n" +
+                                "Введите значение поменьше";
+                        }
+                        else
+                        {
+                            label1.Text = "";
+                            drawFig(AD, BC, pictureBox1, textBox2);
+                        }
+
+                    }
+                }
+                else
+                {
+                    Graphics g = pictureBox1.CreateGraphics();
+                    g.Clear(Color.White);
+                    textBox2.Text = "0";
+                    label1.Text = "Вы ввели нечисловое значение";
+                }
+
+            }
+        }
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length != 0)
+            {
+                if (double.TryParse(textBox1.Text, out double res) == true && double.Parse(textBox1.Text) > 0)
+                {
+                    if (BC < pictureBox1.Width)
+                    {
+                        label1.Text = "";
+                        drawFig(AD, BC, pictureBox1, textBox2);
+                    }
+                }
+                else
+                {
+                    textBox2.Text = "0";
+                    Graphics g = Graphics.FromImage(pictureBox1.Image);
+                    g.Clear(Color.White);
+                }
+            }
+
+        }
+    }
+}
